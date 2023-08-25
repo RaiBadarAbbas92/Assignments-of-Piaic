f006e3c66a5105bdd47dd65c3b13e6ac33e3fa66
Badar Abbas <raibadar37218@gmail.com>
2023-08-25 12:43:40 +0500

    first commit

---
+++ Assignment 1/index.js
@@ -0,0 +1,27 @@
+"use strict";
+// This is my first assignment
+//Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT.
+console.log("Hey Badar Abbas. I have started learning Typescript");
+//Store your name in a variable and print it.
+var myName = "Badar Abbas";
+console.log(myName);
+//Store 10 numbers in different variables. Add all of them and print the SUM.Print the Difference (subtraction). Print the result after multiplying all.
+var a = 2;
+var b = 7;
+var c = 12;
+var d = 17;
+var e = 22;
+var f = 27;
+var g = 32;
+var h = 37;
+var i = 42;
+var j = 47;
+var sum = a + b + c + d + e + f + g + h + i + j;
+var sub = a - b - c - d - e - f - g - h - i - j;
+var mul = a * b * c * d * e * f * g * h * i * j;
+var div = g / a;
+console.log("Sum is", sum);
+console.log("Subtraction is", sub);
+console.log("Multiplication is", mul);
+console.log("Division of g and a is", div);
+console.log(20 - 5 * 16 + 35 / 5);
---
+++ Assignment 1/index.ts
@@ -0,0 +1,32 @@
+// This is my first assignment
+//Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT.
+
+console.log("Hey Badar Abbas. I have started learning Typescript"); 
+
+//Store your name in a variable and print it.
+
+var myName:string="Badar Abbas"
+console.log(myName);
+
+
+//Store 10 numbers in different variables. Add all of them and print the SUM.Print the Difference (subtraction). Print the result after multiplying all.
+var a:number=2
+var b:number=7
+var c:number=12
+var d:number=17
+var e:number=22
+var f:number=27
+var g:number=32
+var h:number=37
+var i:number=42
+var j:number=47
+var sum=a+b+c+d+e+f+g+h+i+j;
+var sub=a-b-c-d-e-f-g-h-i-j;
+var mul=a*b*c*d*e*f*g*h*i*j;
+var div=g/a;
+console.log("Sum is",sum);
+console.log("Subtraction is",sub);
+console.log("Multiplication is",mul);
+console.log("Division of g and a is",div);
+// perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+console.log(20-5*16+35/5);
