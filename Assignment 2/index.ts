5c04024c2fab56eae9e2f612f6d4bff3bc84d844
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2023-08-28 13:28:53 +0500

    This is Second Assignment of Piaic

---
+++ Assignment 2/index.ts
@@ -0,0 +1,44 @@
+//This is my second assignment
+
+// Q:1   Write a program that calculates the area of a rectangle.
+var length:number=15
+var width:number=10
+console.log("area of a rectangle is",width*length);
+
+
+//Q:2  Write a program that checks if a given number is positive, negative, or zero.
+ var givenNumber:number=5
+ if (givenNumber>0) {
+    console.log("The given number is positive ");
+ }
+
+ if(givenNumber<0){
+    console.log("The given number is negitive");
+    
+ }
+ if (givenNumber==0) {
+    console.log("The given number is zero");
+    
+ }
+
+ // Q:3   Write a program that checks if a given number is even or odd.
+
+ var number:number=5
+ if(number%2==0){
+    console.log("This number is Even");
+ }
+ if (number%2!=0) {
+    console.log("This number is Odd");   
+ }
+
+ //Q:4   Write a program that determines if a person is eligible to vote based on their age.
+var age=21
+ if (age>18) {
+    console.log("you are eligible to vote");   
+ }
+ if(age<18){
+    console.log("You are not eligible to vote");
+    
+ }
+
+ 
