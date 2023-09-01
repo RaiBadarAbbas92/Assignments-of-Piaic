09cf944fd453c94c187f6b6a8ab91fe77132469a
Badar Abbas <142784854+RaiBadarAbbas92@users.noreply.github.com>
2023-09-01 12:42:59 +0500

    Assignment 4

---
+++ Assignment 4/index.ts
@@ -0,0 +1,100 @@
+//Create a function that takes an array, an index, and a value as parameters. Inside the function, use the splice method to insert the value at the specified index in the array. Return the modified array.
+function insertValueInArray(array: any[], index: number, value: any): any[] {
+    array.splice(index, 0, value);
+    return array;
+  }
+let array=[1,2,3,4];
+  let index=6
+  let value=5
+  console.log(insertValueInArray(array,index,value));
+  
+
+  //Implement a simple shopping cart program using an array. Create functions to add items, remove items, and update quantities using the splice method. Print the cart's contents after each operation
+  let cart:string[]=["Fruit","Uniform"]
+
+  function additem() {
+    cart.push("Apple")
+    console.log(cart);
+    
+  }
+
+  function remove() {
+    cart.pop()
+    console.log(cart);
+    
+  }
+
+function update() {
+  cart.splice(1,0,'Orange')
+  console.log(cart);
+  
+}
+additem()
+remove()
+update()
+
+
+//Write a program that uses a while loop to print the first 25 integers.
+
+ let i= 1;
+ while (i<=25) {
+  console.log(i);
+  i++;
+  
+ }
+
+// Write a program that uses a while loop to print the first 10 even numbers.
+let I = 1;
+let count = 0;
+while (count < 10) {
+    if (I % 2 === 0) {
+        console.log(I);
+        count++;
+    }
+    I++;
+}
+
+//Create a function that takes a positive integer as parameter and uses a while loop to calculate and return the factorial of that number.
+function factorial(n: number): number {
+  let result = 1;
+  let i = n;
+  while (i > 1) {
+      result *= i;
+      i--;
+  }
+  return result;
+}
+
+// Write a program having an array of numbers if the number is negative it should remove the negative number from the array.
+
+function removeNegativeNumbers(numbers: number[]): number[] {
+    let result: number[] = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] >= 0) {
+            result.push(numbers[i]);
+        }
+    }
+    return result;
+}
+
+// Example usage:
+let numbers = [1, -2, 3, -4, 5];
+numbers = removeNegativeNumbers(numbers);
+console.log(numbers); // [1, 3, 5]
+
+//Create a function that takes an array of numbers as parameter. Use a while loop to calculate and return the sum of all the numbers in the array.
+function sumArray(numbers: number[]): number {
+  let sum = 0;
+  let i = 0;
+  while (i < numbers.length) {
+      sum += numbers[i];
+      i++;
+  }
+  return sum;
+}
+
+
+
+
+
+
